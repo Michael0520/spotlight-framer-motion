e8f50a6557d153afb4b17d2418d8530f5795c89b
Michael0520 <po2319987@gmail.com>
2023-10-09 12:53:20 +0800

    feat: 🎸 update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+/*eslint-env node*/
 module.exports = {
-	content: ['./src/**/*.{js,jsx,ts,tsx}'],
-	theme: {
-		screens: {
-			mobile: { max: '820px' },
-			xs: { max: '640px' },
-			xxs: { max: '370px' },
-		},
-		colors: {
-			background: '#E388AB',
-		},
-		extend: {},
-	},
-	plugins: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      mobile: { max: "820px" },
+      xs: { max: "640px" },
+      xxs: { max: "370px" },
+    },
+    extend: {},
+  },
+  plugins: [],
 };
